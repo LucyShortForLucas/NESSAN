62019f8df52ef1f8ac5a9da3ddc483d9b35d244b
Lucas Schonkeren <Luke.schonkeren@gmail.com>
2026-01-19 14:30:02 +0100

    added comment in bss

--- asm/bss.s
+++ asm/bss.s
@@ -29,7 +29,7 @@
 
 ;; SYSTEM & CORE
 frame_ready: .res 1 ; set to a value != 0 when frame logic is ready to be processed
-current_scene: .res 1
+current_scene: .res 1 ; Enum for which scene to update
 end_state: .res 1 ; to check what type of ending you have
 
 ;; PLAYER PHYSICS
@@ -47,7 +47,7 @@ collision_aabb_9x2: .res 9  ; Max 4 boxes
 
 ;; GAMEPLAY OBJECTS (PICKUPS)   
 list_pickup: .res 10 ; 0th element for how many are active, max 3 coins: 1th for x, 2st for y, 3nd for type, then next coin...
-pickup_timer: .res 1 
+pickup_timer: .res 1 ; A frame-wise timer that determines when new pickups should spawn
 
 ;; GRAPHICS BUFFERS
 clock_draw_buffer: .res 16  ; Buffer for rendering the timer digits.
